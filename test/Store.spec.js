b16099f674dc684394d2a1c4e86bf33b6a82cd4a
chenzhihao <cabtravis@gmail.com>
2017-06-09 23:52:13 +0800

    Store will notify its subscribers when there is an action

--- test/Store.spec.js
+++ test/Store.spec.js
@@ -49,3 +49,13 @@ test('Store can update states from action', t => {
   store.dispatch({type: 'banana'})
   t.deepEqual(store.getStates(), {fruit: 'banana', animal: 'dog'})
 })
+
+test.cb('Store can do broadcast when there is an action', t => {
+  const store = new Store(reducer)
+  store.subscribe(function () {
+    t.deepEqual(store.getStates(), {fruit: 'cherry', animal: 'dog'})
+    t.end()
+  });
+  store.dispatch({type: 'dog'})
+})
+
