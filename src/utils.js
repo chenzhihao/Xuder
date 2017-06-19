7b75eed1f57456f2f3b8bc71d84604fcf1ef5bee
Zhihao <cabtravis@gmail.com>
2017-06-19 23:28:57 +0800

    add compose method

--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,20 @@ export function isString (str) {
 }
 
 /**
+ * Composes single-argument functions from right to left. The rightmost
+ * function can take multiple arguments as it provides the signature for
+ * the resulting composite function.
+ *
+ * @param {...Function} funcs The functions to compose.
+ * @returns {Function} A function obtained by composing the argument functions
+ * from right to left. For example, compose(f, g, h) is identical to doing
+ * (args) => f(g(h(args))).
+ */
+export function compose (...funcs) {
+  return funcs.reduce((acc, func) => (...args) => acc(func(...args)))
+}
+
+/**
  * Shallow compare if two objects have different key or different value for same key(shallowly)
  * @param a Object a
  * @param b Object b
